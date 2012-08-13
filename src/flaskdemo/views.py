b9f3d89d0abf30d899c9ce35c096c0210d190bef
Christian Brink <christianpbrink@gmail.com>
2012-08-13 15:16:36 -0400

    Made our 1st endpoint dynamic, just for demo purposes.

--- src/flaskdemo/views.py
+++ src/flaskdemo/views.py
@@ -1,11 +1,12 @@
 # Imports an object that was initialized in __init__.py. 
 from flaskdemo import app
-
+from flask import request
 
 ##
 ## Our pages
 ##
 
-@app.route('/contacts')
-def index():
-  return 'Adrianne: (917) 555-9876'
+@app.route('/contacts/<areacode>')
+def index(areacode='917'):
+  contact = request.args.get('name', 'Adrianne')
+  return '{}: ({}) 555-9876'.format(contact, areacode)
